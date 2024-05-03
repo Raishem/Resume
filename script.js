ad9f9c59a7d92c4eaa95de352d400651b2fee062
CIDKAGENOU\Shem John Juevesano <sjjuevesano@gmail.com>
2024-05-03 16:11:35 +0800

    redo of kneeger

--- script.js
+++ script.js
@@ -2,37 +2,56 @@
     fetch('https://resumewebservice.onrender.com/')
     .then (response => response.json())
     .then (data => {
-        document.getElementById('level').innerText = data.education.level;
-        document.getElementById('school').innerText = data.education.school;
-        document.getElementById('year').innerText = data.education.year;
-        document.getElementById('schooladdress').innerText = data.education.schooladdress;
-        document.getElementById('course').innerText = data.education.course;
+        
+        document.getElementById('name').innerText = data.personal_info.name;
+        document.getElementById('address').innerText = "Address: " + data.personal_info.address;
+        document.getElementById('contactno').innerText = "Contact Number: " + data.personal_info.contactno;
+        document.getElementById('email').innerText = "Email Address: " + data.personal_info.email;
+        document.getElementById('birthdate').innerText = "Birthdate: " + data.personal_info.birthdate;
+        document.getElementById('age').innerText = "Age: " + data.personal_info.age;
+        document.getElementById('gender').innerText = "Gender: " + data.personal_info.gender;
 
-        const skills = document.getElementById('skills');
+        document.getElementById('about_me').innerText = data.about_me;
+
+        const skillslist = document.getElementById('skills');
         data.skills.forEach(skill => {
             const li = document.createElement('li');
             li.innerText = skill;
-
-            skills.appendChild(li);
+            skillslist.appendChild(li);
         });
 
-        const experience = document.getElementById('experience');
+        const educationContainer = document.getElementById('education');
+        data.education.forEach(edu => {
+            const eduDiv = document.createElement('div');
+            eduDiv.innerHTML = `
+            <h2>${edu.level}<h2>
+            <p>${edu.school}<p>
+            <p>${edu.address}<p>
+            `;
+            educationContainer.appendChild(eduDiv);
+        });
 
-        data.experienceforEach(exp => {
+        const experienceList = document.getElementById('experience');
+        data.experience.forEach(exp => {
             const li = document.createElement('li');
             li.innerText = exp;
-
-            experience.appendChild(li);
+            experienceList.appendChild(li);
         });
 
-        document.getElementById('name').innerText=data.personal_info.name;
-        document.getElementById('Address').innerText=data.personal_info.Address;
-        document.getElementById('ContactNo').innerText=data.personal_info.ContactNo;
-        document.getElementById('Email').innerText=data.personal_info.Email;
-        document.getElementById('Birthdate').innerText=data.personal_info.Birthdate;
-        document.getElementById('Age').innerText=data.personal_info.Age;
-        document.getElementById('Gender').innerText=data.personal_info.Gender;
+        const awardsList = document.getElementById('awards');
+        data.awards.forEach(award => {
+            const li = document.createElement('li');
+            li.innerText = award;
+            awardsList.appendChild(li);
+        });
+
+        const referencesList = document.getElementById('references');
+        data.references.forEach(reference => {
+            li.innerText = reference;
+            referencesList.appendChild(li);
+        });
+
     })
     .catch(error => {
-        console.error('Error fetching data:',error);
+        console.error('Error fetching data:', error);
     });
